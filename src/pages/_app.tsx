5254e7c0a0a21e80e7bb630fd3c8120a781b2b85
Thiago Moraes <user000.000resu@gmail.com>
2021-07-27 16:43:54 -0300

    Create Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "../../styles/global";
+import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Header />
 			<Component {...pageProps} />
 			<GlobalStyles />
 		</>
