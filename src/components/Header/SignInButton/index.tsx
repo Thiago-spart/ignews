4f7a4c9377775540343299b882bb907b32ff17cb
Thiago Moraes <user000.000resu@gmail.com>
2021-09-04 20:03:20 -0300

    Add fauna data-base

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,25 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import { Button } from "./styles";
 
 export const SignInButton = () => {
-	const isUserLoggedIn = true;
+	const [session] = useSession();
 
-	return isUserLoggedIn ? (
-		<Button type="button">
+	return session ? (
+		<Button type="button" onClick={() => signOut()}>
 			<FaGithub color="#84d361" />
-			Nome usuario
+			{session.user?.name}
 			<FiX color="#737380" className="closeIcon" />
 		</Button>
 	) : (
-		<Button type="button">
+		<Button
+			type="button"
+			onClick={() => {
+				signIn("github");
+			}}
+		>
 			<FaGithub color="#eba417" />
 			Sign in with Github
 		</Button>
